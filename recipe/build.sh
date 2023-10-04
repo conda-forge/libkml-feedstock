b20077c404e8163150e212e40c5c9fe257ec9ac4
H. Vetinari <h.vetinari@gmx.com>
2023-10-04 20:01:34 +1100

    actually enable library tests

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,6 +4,10 @@ set -ex
 mkdir build
 cd build
 
+if [[ "${build_platform}" == "${target_platform}" ]]; then
+    export CMAKE_ARGS="${CMAKE_ARGS} -DBUILD_TESTING=ON"
+fi
+
 cmake ${CMAKE_ARGS} \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
     ..
