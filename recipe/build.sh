02fd5051ddb4df6c2aabe859799f33a755b14872
Isuru Fernando <isuruf@gmail.com>
2024-07-09 22:56:40 -0500

    separate libkml-devel output

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,6 +8,12 @@ if [[ "${build_platform}" == "${target_platform}" ]]; then
     export CMAKE_ARGS="${CMAKE_ARGS} -DBUILD_TESTING=ON"
 fi
 
+if [[ "${target_platform}" == "linux-"* ]]; then
+    export LDFLAGS="$LDFLAGS -Wl,--exclude-libs,ALL"
+else
+    export LDFLAGS="$LDFLAGS -Wl,-unexported_symbol,_zip*"
+fi
+
 cmake ${CMAKE_ARGS} \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
     ..
@@ -17,4 +23,8 @@ cmake --build .
 if [[ "${build_platform}" == "${target_platform}" ]]; then
     DYLD_FALLBACK_LIBRARY_PATH=${PREFIX}/lib ctest --progress --output-on-failure
 fi
+
 make install -j $CPU_COUNT
+
+rm -rf $PREFIX/include/minizip
+rm -rf $PREFIX/lib/libminizip*
